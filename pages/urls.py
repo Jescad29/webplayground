43ad6281e8bf8a9dea58bc403375270f79d09492
Jesus Cadena <jesuscadena27@hotmail.com>
2024-01-11 14:51:13 -0600

    UpdateView

--- pages/urls.py
+++ pages/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import PageCreate, PageListView, PageDetailView
+from .views import PageCreate, PageListView, PageDetailView, PageUpdate
 
 pages_patterns = ([
     path('', PageListView.as_view(), name='pages'),
     path('<int:pk>/<slug:slug>/',
          PageDetailView.as_view(), name='page'),
     path('create/', PageCreate.as_view(), name='create'),
+    path('update/<int:pk>/', PageUpdate.as_view(), name='update'),
 ], 'pages')
