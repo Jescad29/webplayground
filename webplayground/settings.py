15a2380ed4d8e6242aecf94e04d3d75dfdf66fec
Jesus Cadena <jesuscadena27@hotmail.com>
2024-01-14 00:52:13 -0600

    Cerrar sesion (logout)

--- webplayground/settings.py
+++ webplayground/settings.py
@@ -122,8 +122,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 # Auth redirect
-LOGIN_REDIRECT_URL = 'home'
-
+LOGIN_REDIRECT_URL = 'pages:pages'
+LOGOUT_REDIRECT_URL = 'home'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
