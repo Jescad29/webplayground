629c1b531d4dbe7ae2358c2669282fcc375e0421
Jesus Cadena <jesuscadena27@hotmail.com>
2024-01-16 00:23:12 -0600

    Conf-serv-smtp Restauracion contraseña

--- webplayground/settings.py
+++ webplayground/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -127,5 +128,14 @@ LOGOUT_REDIRECT_URL = 'home'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+
+# Emails
+if DEBUG:
+    EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+    EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
+else:
+    # Aquí hay que configurar un email real para producción
+    pass
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
