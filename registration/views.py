d9e87a2718c11053fb6eeddd26da1c6ce00f5096
Jesus Cadena <jesuscadena27@hotmail.com>
2024-01-15 23:50:22 -0600

    email obligatorio y validado

--- registration/views.py
+++ registration/views.py
@@ -1,3 +1,4 @@
+from .forms import UserCreationFormWithEmail
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
@@ -6,7 +7,7 @@ from django import forms
 
 
 class SignUpView(CreateView):
-    form_class = UserCreationForm
+    form_class = UserCreationFormWithEmail
     template_name = 'registration/signup.html'
 
     def get_success_url(self):
@@ -17,6 +18,8 @@ class SignUpView(CreateView):
         # Modificar en tiempo real el formulario
         form.fields['username'].widget = forms.TextInput(
             attrs={'class': 'form-control mb-2', 'placeholder': 'Nombre de usuario'})
+        form.fields['email'].widget = forms.EmailInput(
+            attrs={'class': 'form-control mb-2', 'placeholder': 'Dirección email'})
         form.fields['password1'].widget = forms.PasswordInput(
             attrs={'class': 'form-control mb-2', 'placeholder': 'Contraseña'})
         form.fields['password2'].widget = forms.PasswordInput(
