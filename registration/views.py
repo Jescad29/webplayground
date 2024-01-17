c22cea7e6c20cd13460e44e84a5373ab7f5fea9a
Jesus Cadena <jesuscadena27@hotmail.com>
2024-01-16 20:30:27 -0600

    edit-perfil

--- registration/views.py
+++ registration/views.py
@@ -1,11 +1,15 @@
+from typing import Any
+from django.db.models.base import Model as Model
+from django.db.models.query import QuerySet
 from .forms import UserCreationFormWithEmail
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import CreateView
-from django.views.generic.base import TemplateView
+from django.views.generic.edit import UpdateView
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse_lazy
 from django import forms
+from .models import Profile
 # Create your views here.
 
 
@@ -31,5 +35,13 @@ class SignUpView(CreateView):
 
 
 @method_decorator(login_required, name='dispatch')
-class ProfileUpdate(TemplateView):
+class ProfileUpdate(UpdateView):
+    model = Profile
+    fields = ['avatar', 'bio', 'link']
+    success_url = reverse_lazy('profile')
     template_name = 'registration/profile_form.html'
+
+    def get_object(self):
+        # Recuperar el objeto que se va a editar
+        profile, create = Profile.objects.get_or_create(user=self.request.user)
+        return profile
