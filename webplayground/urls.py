0f23c08fb00eb2c512c1d65109adb83dd63cf4c5
Jesus Cadena <jesuscadena27@hotmail.com>
2024-01-17 02:17:49 -0600

    app-profiles

--- webplayground/urls.py
+++ webplayground/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from pages.urls import pages_patterns
 from django.conf import settings
+from profiles.urls import profiles_patterns
 
 urlpatterns = [
     path('', include('core.urls')),
@@ -26,6 +27,7 @@ urlpatterns = [
     # Incluimos las urls de autenticación de Django
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('registration.urls')),
+    path('profiles/', include(profiles_patterns)),
 
 ]
 
