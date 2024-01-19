3fa1273e7573e75e94272f3031de79f44bb89dcf
Jesus Cadena <jesuscadena27@hotmail.com>
2024-01-19 02:07:55 -0600

    migraciones

--- messenger/views.py
+++ messenger/views.py
@@ -1,10 +1,12 @@
 from django.http import Http404, JsonResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic import TemplateView
 from .models import Thread, Message
 from django.shortcuts import get_object_or_404
+from django.contrib.auth.models import User
 
 
 from django.contrib.auth.decorators import login_required
@@ -38,6 +40,15 @@ def add_message(request, pk):
                 user=request.user, content=content)
             thread.messages.add(message)
             json_response['created'] = True
+            if len(thread.messages.all()) is 1:
+                json_response['first'] = True
     else:
         raise Http404("user is not authenticated")
     return JsonResponse(json_response)
+
+
+@login_required
+def start_thread(request, username):
+    user = get_object_or_404(User, username=username)
+    thread = Thread.objects.find_or_create(user, request.user)
+    return redirect(reverse_lazy('messenger:detail', args=[thread.pk]))
